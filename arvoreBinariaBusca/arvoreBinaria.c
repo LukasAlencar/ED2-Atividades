2a664bf31923aa91383f5cb38f8f9d22023019d9
LucasAlencar1321 <lucas.alencar@intelly.com.br>
2022-09-19 17:12:52 -0300

    atividade da arvore binaria de busca

---
+++ arvoreBinariaBusca/arvoreBinaria.c
@@ -0,0 +1,217 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvoreBinaria.h"
+
+struct NO{
+    int info;
+    struct NO *esq;
+    struct NO *dir;
+};
+
+ArvBin *cria_arvBin(){
+    ArvBin *raiz = (ArvBin*) malloc(sizeof(ArvBin));
+    if(raiz != NULL){
+        *raiz = NULL;
+    }
+    return raiz;
+}
+
+void liberar_arvBin(ArvBin *raiz){
+    if(raiz == NULL){
+        return;
+    }
+    libera_NO(*raiz);
+    free(raiz);
+}
+
+void libera_NO(struct NO *no){
+    if(no == NULL){
+        return;
+    }
+    libera_NO(no->esq);
+    libera_NO(no->dir);
+    free(no);
+    no = NULL;
+}
+
+
+int vazia_arvBin(ArvBin *raiz){
+    if(raiz == NULL){
+        return 1;
+    }
+    if(*raiz == NULL){
+        return 1;
+    }
+    return 0;
+}
+
+int altura_arvBin(ArvBin *raiz){
+    if(raiz == NULL){
+        return 0;
+    }
+    if(*raiz == NULL){
+        return 0;
+    }
+    int alt_esq = altura_arvBin(&((*raiz)->esq));
+    int alt_dir = altura_arvBin(&((*raiz)->dir));
+
+    if(alt_esq > alt_dir){
+        return(alt_esq + 1);
+    }else{
+        return(alt_dir + 1);
+    }
+}
+
+int totalNO_arvBin(ArvBin *raiz){
+    if(raiz == NULL){
+        return 0;
+    }
+    if(*raiz == NULL){
+        return 0;
+    }
+
+    int alt_esq = totalNO_arvBin(&((*raiz)->esq));
+    int alt_dir = totalNO_arvBin(&((*raiz)->dir));
+    return(alt_esq + alt_dir + 1);
+}
+
+void preOrdem_arvBin(ArvBin *raiz){
+    if(raiz == NULL){
+        return;
+    }
+    if(*raiz != NULL){
+        printf("%d\n", (*raiz)->info);
+        preOrdem_arvBin(&((*raiz)->esq));
+        preOrdem_arvBin(&((*raiz)->dir));
+
+    }
+}
+
+void emOrdem_arvBin(ArvBin *raiz){
+    if(raiz == NULL){
+        return;
+    }
+    if(raiz != NULL){
+        emOrdem_arvBin(&((*raiz)->esq));
+        printf("%d\n", (*raiz) -> info);
+        emOrdem_arvBin(&((*raiz)->esq));
+    }
+}
+
+void posOrdem_arvBin(ArvBin *raiz){
+    if(raiz == NULL){
+        return;
+    }
+    if(*raiz != NULL){
+        posOrdem_arvBin(&((*raiz)->esq));
+        posOrdem_arvBin(&((*raiz)->dir));
+        printf("%d\n", (*raiz)-> info);
+    }
+}
+
+int insere_arvBin(ArvBin *raiz, int valor){
+    if(raiz == NULL){
+        return 0;
+    }
+    struct NO *novo;
+    novo = (struct NO*) malloc(sizeof(struct NO));
+    if(novo == NULL){
+        return 0;
+    }
+    novo->info = valor;
+    novo->dir = NULL;
+    novo->esq = NULL;
+    if(*raiz == NULL){
+        *raiz = novo;
+    }else{
+        struct NO *atual = *raiz;
+        struct NO *ant = NULL;
+        while(atual != NULL){
+            ant = atual;
+            if(valor == atual->info){
+                free(novo);
+                return 0;
+            }
+            if(valor > atual->info){
+                atual = atual->dir;
+            }else{
+                atual = atual->esq;
+            }
+
+        }
+        if(valor > ant->info){
+            ant->dir = novo;
+        }else{
+            ant->esq = novo;
+        }
+    }
+    return 1;
+}
+
+struct NO *remove_atual(struct NO *atual){
+        struct NO *no1, *no2;
+        if(atual->esq == NULL){
+            no2 = atual->dir;
+            free(atual);
+            return no2;
+        }
+        no1 = atual;
+        no2 = atual->esq;
+        while(no2->dir != NULL){
+            no1 = no2;
+            no2 = no2->dir;
+        }
+        if(no1 != atual){
+            no1->dir = no2->esq;
+            no2->esq = atual->esq;
+        }
+        no2->dir = atual->dir;
+        free(atual);
+        return no2;
+}
+
+int remove_arvBin(ArvBin *raiz, int valor){
+    if(raiz == NULL){
+        return 0;
+    }
+    struct NO *ant = NULL;
+    struct NO *atual = *raiz;
+    while (atual != NULL){
+        if(valor == atual->info){
+            if(atual == *raiz){
+                *raiz == remove_atual(atual);
+            }else{
+                if(ant->dir == atual){
+                    ant->dir = remove_atual(atual);
+                }else{
+                    ant->esq = remove_atual(atual);
+                }
+            }
+            return 1;
+        }
+        ant = atual;
+        if(valor > atual->info){
+            atual = atual->dir;
+        }else{
+            atual = atual->esq;
+        }
+    }
+}
+
+int consulta_arvBin(ArvBin *raiz, int valor){
+    if(raiz == NULL){
+        return 0;
+    }
+    struct NO *atual = *raiz;
+    while(atual != NULL){
+        if(valor== atual->info){
+            return 1;
+        }
+        if(valor > atual->info){
+            atual = atual->dir;
+        }else{
+            atual = atual->esq;
+        }
+    }
+    return 0;
+}
---
+++ arvoreBinariaBusca/arvoreBinaria.h
@@ -0,0 +1,23 @@
+typedef struct NO *ArvBin;
+
+ArvBin *cria_arvBin();
+
+void liberar_arvBin(ArvBin *raiz);
+
+int vazia_arvBin(ArvBin *raiz);
+
+int altura_arvBin(ArvBin *raiz);
+
+int totalNO_arvBin(ArvBin *raiz);
+
+void preOrdem_arvBin(ArvBin *raiz);
+
+void emOrdem_arvBin(ArvBin *raiz);
+
+void posOrdem_arvBin(ArvBin *raiz);
+
+int insere_arvBin(ArvBin *raiz, int valor);
+
+int remove_arvBin(ArvBin *raiz, int valor);
+
+int consulta_arvBin(ArvBin *raiz, int valor);
